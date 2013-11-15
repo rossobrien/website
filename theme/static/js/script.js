c5064665237ea1be6f74dd9ea6ce3092fbcb43d6
rossobrien <ross.obrien@gmail.com>
2013-11-14 23:35:11 -0800

    style tweaks

--- theme/static/js/script.js
+++ theme/static/js/script.js
@@ -7,6 +7,11 @@ $( document ).ready(function() {
 
 });
 
+$('.nav_box').on("click", function() {
+	window.open($(this).find('a').attr('href'), "_self");
+    return false;
+});
+
 $("#blog").on("click", function() {
 	var pathname = window.location.pathname;
 	if (pathname == "/") 
